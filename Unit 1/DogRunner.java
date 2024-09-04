90fbe5d94c0905b6db1d0237c585692b703ecb8e
Atharva Mishra <atharvamishra956+1@gmail.com>
2024-09-03 19:58:15 -0500

    finished dog runner lab

--- Unit 1/DogRunner.java
+++ Unit 1/DogRunner.java
@@ -1,9 +1,29 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
+//Name - Atharva Mishra
+//Class - Patterson 3B
+//Description - Makes a Dog object with a name and age and can increase the age by one, get the age, get the people age, and get the name
 
 public class DogRunner {
-	public static void main( String[] args ) {
-		//add test cases	
-		//use the examples on the lab handout	
+	public static void main(String[] args) {
+		Dog d = new Dog("Rover");
+		System.out.println(d.getPeopleAge(7));
+
+		d.increaseAgeByOne();
+		d.increaseAgeByOne();
+
+		System.out.println(d.getPeopleAge(11));
+		System.out.println(d.getName());
+
+		d.increaseAgeByOne();
+		d.increaseAgeByOne();
+		d.increaseAgeByOne();
+		d.increaseAgeByOne();
+
+		System.out.println(d.getPeopleAge(8));
+
+		Dog g = new Dog("bob", 3);
+		System.out.println(g.getName());
+		System.out.println(g.getPeopleAge(5));
 	}
 }
